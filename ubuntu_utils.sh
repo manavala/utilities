3ecc778bb4718662e023a2df27759997b46c28d0
manavala <gvmanavalan@gmail.com>
2016-09-22 22:48:53 +0530

    Update ubuntu_utils.sh

--- ubuntu_utils.sh
+++ ubuntu_utils.sh
@@ -5,3 +5,9 @@ gsettings set org.gnome.desktop.wm.preferences button-layout ':minimize,maximize
 
 # move buttons to the left
 gsettings set org.gnome.desktop.wm.preferences button-layout 'minimize,maximize,close:'
+
+
+lap_screen_off() {
+     echo "first check with xrandr option"
+     echo "xrandr --output LVDS1 --off"
+ }
