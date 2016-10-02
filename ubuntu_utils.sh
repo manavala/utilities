2861f9949b4ffdffd43e162b0e0c06f7af1540bf
manavala <gvmanavalan@gmail.com>
2016-10-02 18:08:45 +0530

    Update ubuntu_utils.sh

--- ubuntu_utils.sh
+++ ubuntu_utils.sh
@@ -16,3 +16,23 @@ lap_screen_off() {
  all_screen_off() {
       echo "xset dpms force off"
  }
+
+
+limit_internet_speed() {
+
+     echo "option 1 (per interface)"
+     echo "to install"
+     echo "sudo apt-get install wondershaper"
+     echo "eg: check with ifconfig # downspeed is 1024 kb/s, up is 256 kb/s"
+     echo "sudo wondershaper eth1 1024 256"
+     echo "to reset 'sudo wondershaper clear eth1' "
+
+     echo "option 2 (automatically choose any interface(wifi/eth/usb/etc) that using internet)"
+     echo "to install"
+     echo "sudo apt-get install trickle"
+     echo "sudo trickle -d 120 -u 32 ie down 120 KB/s down 32 KB/s" 
+
+     echo "For any more info: man wondershaper or man trickle"
+}
+
+
