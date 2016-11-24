0f412b1457b95d83997c92bfcb81d6577f7e2ab4
Manavalan Venugopal <gvmanavalan@gmail.com>
2016-11-24 19:23:21 +0530

    Update linux_utils.h

--- linux_utils.h
+++ linux_utils.h
@@ -55,3 +55,9 @@ while sleep 1;do tput sc;tput cup 0 $(($(tput cols)-11));echo -e "\e[31m`date +%
 #For error "Cannot remove Device busy"
 lsof +D /path/
 kill -9 pid
+
+
+#get number of cores
+nproc # returns only cpu count
+lspcu # resturs minimal info
+cat /proc/cpuinfo #explins about each cores
