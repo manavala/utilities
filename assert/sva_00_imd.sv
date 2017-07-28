4917f049aa3cbfd09bb84395926a7b998b475f85
Manavalan Venugopal <gvmanavalan@gmail.com>
2017-07-28 16:24:57 +0800

    Update sva_00_imd.sv

--- assert/sva_00_imd.sv
+++ assert/sva_00_imd.sv
@@ -1,10 +1,13 @@
-package sva_lib_test;
+package sva_lib_test;                                                                                
 
-  sequence sr1(req,gnt);
+  sequence sr1(req, gnt);
+    //clock can be speciefied here also
+    //should be edge, cannot be latch, can be gated clk like (clk && slct_sig)
     req ##2 gnt;
   endsequence
 
-  property pr1(clk, req, gnt, start);
+  property pr1(clk, req, gnt, start); //Formal Arguments
+    //values are "sampled" at 'preponed region' == 'value from previous time slot'
     @(posedge clk) start |-> sr1(req, gnt);
   endproperty
 
@@ -15,19 +18,26 @@ module test;
   import sva_lib_test::*;
 
   bit clk;
-  bit start, req, gnt;
+  bit start, req;
+  reg gnt=0;
 
   initial forever #5 clk = !clk;
 
-  chk_req_gnt: assert property ( pr1(clk, req, gnt, start) ) $display(" [%0t] pass",$realtime);
-                                 else $display(" [%0t] fail",$realtime);                             
+  //Assertions are evaluated in observed region
+  chk_req_gnt: assert property ( pr1(clk, req, gnt, start) ) //Actual Arguments 
+                  $display(" [%0t] pass",$realtime); //Action blocks - Evaluated at Reactive region
+               else
+                 $display(" [%0t] fail",$realtime); //Action blocks - Evaluated at Reactive region
+
   initial begin
     stim(1);//pass
-    stim(0);//fail
+    stim(0);//fail //x,z,0 are evaluated as fail
+    stim(1'bx);//fail
+    stim(1'bz);//fail
     @(posedge clk);
     $finish;
   end
-
+  
   task stim(input bit stat);
     @(posedge clk);
     start = 1;
@@ -39,7 +49,7 @@ module test;
     gnt = stat;
     @(posedge clk);
   endtask
-
+  
   initial $monitor(" [%0t] clk %0d start=%0d req=%0d gnt=%0d",$realtime,clk,start,req,gnt);
 
 endmodule
