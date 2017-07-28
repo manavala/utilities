7a018537c28cec262a68384ed6319495bab7afff
Manavalan Venugopal <gvmanavalan@gmail.com>
2017-07-28 16:58:08 +0800

    Update sva_01_def_clk.sv

--- assert/sva_01_def_clk.sv
+++ assert/sva_01_def_clk.sv
@@ -1,4 +1,4 @@
-ackage sva_lib_test;
+package sva_lib_test;                                                                                                
 
   sequence sr1(req, gnt);
     //clock can be speciefied here also
@@ -17,17 +17,25 @@ module test;
 
   import sva_lib_test::*;
 
-  bit clk;
+  bit clk, clk2;
   bit start, req;
   reg gnt=0;
 
   initial forever #5 clk = !clk;
 
+  assign clk2 = ~clk;
+
   //Assertions are evaluated in observed region
+  //uses default clk
   chk_req_gnt: assert property ( pr1(req, gnt, start) ) //Actual Arguments 
                   $display(" [%0t] pass",$realtime); //Action blocks - Evaluated at Reactive region
                else
                  $display(" [%0t] fail",$realtime); //Action blocks - Evaluated at Reactive region
+  //uses cb2 clk
+  chk_st_req: assert property ( @cb2 start |-> req )
+                  $display(" [%0t] pass 1",$realtime);
+               else
+                 $display(" [%0t] fail 1",$realtime);
 
   initial begin
     stim(1);//pass
@@ -38,11 +46,15 @@ module test;
     $finish;
   end
 
-  //clock automatically inherits to property, sequences                                              
+  //clock automatically inherits to property, sequences which doesn't have overrite clock
   default clocking cb1
     @(posedge clk);
   endclocking
-  
+
+  clocking cb2
+    @(posedge clk2);
+  endclocking
+
   task stim(input bit stat);
     @(posedge clk);
     start = 1;
@@ -54,7 +66,7 @@ module test;
     gnt = stat;
     @(posedge clk);
   endtask
-  
+
   initial $monitor(" [%0t] clk %0d start=%0d req=%0d gnt=%0d",$realtime,clk,start,req,gnt);
 
 endmodule
