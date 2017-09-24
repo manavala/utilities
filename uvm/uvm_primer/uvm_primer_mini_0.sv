03d1173b03a25bbe97722aaa6422ae4a6be22507
Manavalan Venugopal <gvmanavalan@gmail.com>
2017-09-24 23:08:58 +0800

    Update uvm_primer_mini_0.sv

--- uvm/uvm_primer/uvm_primer_mini_0.sv
+++ uvm/uvm_primer/uvm_primer_mini_0.sv
@@ -166,22 +166,44 @@ endclass
 **       2. create data part sequences and which may be deleted once
 **          sent and not required anymore to save memory
 *******************************************************************/
+/*******************************************************************
+** test: 1. instantiate structure componet(env and its inside)
+**          which will be available throught sim
+**       2. create data part sequences and which may be deleted once
+**          sent and not required anymore to save memory
+*******************************************************************/
 class base_test extends uvm_test;
   `uvm_component_utils(base_test);
   sequencer seqr_h;
   env env_h;
-  seq seq_h;
   function new(string name, uvm_component parent);
     super.new(name,parent);
   endfunction
   function void build_phase(uvm_phase phase);
     env_h = env::type_id::create("env_h",this);
-    seq_h = seq::type_id::create("seq_h");
   endfunction
   function void end_of_elaboration_phase(uvm_phase phase);
     //this doesn't need casting
     if(!$cast(seqr_h,uvm_top.find("*.env_h.agnt_h.seqr_h")))
       $fatal("can't copy handle");
+  endfunction
+endclass
+
+/*******************************************************************
+** test: just to 'start' actual sequence for actual test
+*******************************************************************/
+class test extends base_test;
+  `uvm_component_utils(test);
+  seq seq_h;
+  function new(string name,uvm_component parent);
+    super.new(name,parent);
+  endfunction
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    seq_h = seq::type_id::create("seq_h");
+  endfunction
+  function void end_of_elaboration_phase(uvm_phase phase);
+    super.end_of_elaboration_phase(phase);
     uvm_top.print_topology();
   endfunction
   task run_phase(uvm_phase phase);
@@ -192,8 +214,9 @@ class base_test extends uvm_test;
   endtask
 endclass
 
+
 module top;
   initial begin
-    run_test("base_test");
+    run_test("test");
   end
 endmodule
