8bf80eb053fcc3665ed6505d6504ae305981d0f3
Manavalan Venugopal <gvmanavalan@gmail.com>
2017-10-23 17:03:17 +0800

    Create cb.py

---
+++ callback_eg/cb.py
@@ -0,0 +1,21 @@
+## callback
+## pass the "function" as the argument to other function
+
+#!/usr/bin/env python
+
+def my_square(val):
+  return val ** 2;
+
+def caller(val,func):
+  return func(val)
+
+for i in range(5):
+  j=caller(i,my_square)
+  print "# %s %s" % (i, j)
+
+#output
+#0 0
+#1 1
+#2 4
+#3 9
+#4 16
