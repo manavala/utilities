55fd933c63c0859f9b1b5a1b90a20f7f3d6763c7
Manavalan Venugopal <gvmanavalan@gmail.com>
2017-12-03 17:57:57 +0800

    Update logic_var.sv

--- codes/sv/logic_var.sv
+++ codes/sv/logic_var.sv
@@ -1,12 +1,25 @@
 program test;
 
+  //logic variable
   //at same time, if two drivers writes to logic, only last assignment wins
   //operation opposite to reg/wire where if two drivers with different values will not result in x
   //assignment similar to reg/wire ie continuous or procedural
   //cannot be used for tri-state assignment
-  logic a;
+  logic a=0;
+  reg b=0;
+  wire c=0;
+  
+  assign c = 0;
+  assign c = 1;
 
-  initial a=0;
-  initial a=1;
+  initial begin
+    a=0;
+    b=0;
+  end
+  
+  initial begin
+    a=1;
+    b=1;
+  end
 
 endprogram
