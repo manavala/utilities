52a59f829102f1fbbc55d59907de40b3a463b6ff
Manavalan Venugopal <gvmanavalan@gmail.com>
2018-05-21 15:21:39 +0100

    Update print_check1.vhd

--- vhdl/print_check1.vhd
+++ vhdl/print_check1.vhd
@@ -1,7 +1,9 @@
-- hello_world.vhdl  Just output to the screen
+--in questa, vcom file_name; vsim -c top_entity_name -do 'run -all; exit'
+
+-- hello_world.vhdl  Just output to the screen
 --                   This should be independent of whose VHDL you use
 --                   When using some vendors GUI, you have a learning curve
---                   Using portable VHDL, it will run on all vendors
+--                   Using porta--in questa, vcom file_name; vsim -c top_entity_name -do 'run -all; exit'ble VHDL, it will run on all vendors
 --                   with implementations conforming to IEEE Std. 1076-1993
 
 
