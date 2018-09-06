6c96d3258c3c371c750d2a34f32721e3b10f00d0
Manavalan Venugopal <gvmanavalan@gmail.com>
2018-09-06 21:51:09 +0100

    Update simple.sv

--- uvm/check/simple.sv
+++ uvm/check/simple.sv
@@ -1,8 +1,10 @@
 //##########
 //command: vcs -ntb_opts uvm -sverilog simple.sv ; ./simv
 //Questa
-//vlog simple.sv +incdir+$UVM_HOME/src $UVM_HOME/src/uvm_pkg.sv
-//vsim -c top -do "run -all; exit" -sv_lib $UVM_HOME/../../uvm-1.2/linux_x86_64/uvm_dpi
+//export UVM_HOME=$QUESTA_HOME/verilog_src/uvm-1.2/src
+//export QUESTA_UVM_HOME=$QUESTA_HOME/verilog_src/questa_uvm_pkg-1.2/src
+//vlog simple.sv +incdir+$UVM_HOME/src $UVM_HOME/src/uvm_pkg.sv +incdir+$QUESTA_UVM_HOME $QUESTA_UVM_HOME/questa_uvm_pkg.sv
+//vsim -c top -do "run -all; exit" -sv_lib $UVM_HOME/../../uvm-1.2/linux_x86_64/uvm_dpi 
 //############
 
 import uvm_pkg::*;
