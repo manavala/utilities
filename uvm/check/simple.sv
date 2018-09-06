0dbad26cef9f66c2ebf8ad68b39325f04a7e08ab
Manavalan Venugopal <gvmanavalan@gmail.com>
2018-09-06 21:10:18 +0100

    Update simple.sv

--- uvm/check/simple.sv
+++ uvm/check/simple.sv
@@ -2,7 +2,7 @@
 //command: vcs -ntb_opts uvm -sverilog simple.sv ; ./simv
 //Questa
 //vlog simple.sv +incdir+$UVM_HOME/src $UVM_HOME/src/uvm_pkg.sv
-//vsim -c top -do "run -all; exit" -sv_lib $UVM_HOME/../uvm-1.2/linux_x86_64/uvm_dpi
+//vsim -c top -do "run -all; exit" -sv_lib $UVM_HOME/../../uvm-1.2/linux_x86_64/uvm_dpi
 //############
 
 import uvm_pkg::*;
