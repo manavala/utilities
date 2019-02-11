1385cc18f8b543a722cc5cc65559ed2f932e0167
Manavalan Venugopal <gvmanavalan@gmail.com>
2019-02-11 21:58:12 +0000

    Update thread_eg.py

--- python/thread_eg.py
+++ python/thread_eg.py
@@ -1,4 +1,4 @@
-from multiprocessing import Process 
+from threading import Thread 
 import time 
 import sys 
  
@@ -6,34 +6,25 @@ def func1(a,b):
   print 'func1: starting 1 sec' 
   delay = a*b 
   time.sleep(delay) 
-  print ('func1: finishing ', delay) 
+  print ('func1: finishing',delay) 
  
 def func2(a,b): 
   print 'func2: starting 2 sec' 
   delay = a*b*2 
   time.sleep(delay) 
-  print ('func2: finishing ',delay) 
- 
-def runInParallel(*fns): 
-  proc = [] 
-  for fn in fns: 
-    p = Process(target=fn) 
-    p.start() 
-    proc.append(p) 
-  for p in proc: 
-    p.join() 
+  print ( 'func2: finishing', delay) 
  
 def run(run_list): 
-  proc_list = [] 
+  threads = [] 
   for ii in run_list: 
       # We start one thread per url present. 
       print ii["func"], ii["arg1"], ii["arg2"] 
-      process = Process(target=ii["func"], args=[ii["arg1"], ii["arg2"]]) 
+      process = Thread(target=ii["func"], args=[ii["arg1"], ii["arg2"]]) 
       process.start() 
-      proc_list.append(process) 
+      threads.append(process) 
   # We now pause execution on the main thread by 'joining' all of our started threads. 
   # This ensures that each has finished processing the urls. 
-  for process in proc_list: 
+  for process in threads: 
       process.join() 
    
 if __name__ == '__main__': 
